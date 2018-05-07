9d3d6ce1387712b8cd49aa6c6d57b0430a394f90
Marcel Jankrift <marcel@jankrift.de>
2018-05-07 17:52:30 +0200

    Add google maps api example

--- map_server.js
+++ map_server.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
+app.use(express.static('map'));
+
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+
 var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
