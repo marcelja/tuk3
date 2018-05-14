756d71e20fa9ec6a82ae7a5f00e5ce68c20cf3b8
Toni Stachewicz <toni.stachewicz@student.hpi.de>
2018-05-14 15:01:44 +0200

    added route for new procedure

--- server.py
+++ server.py
@@ -5,13 +5,23 @@ from flask import Flask
 app = Flask(__name__)
 
 SCHEMA_NAME = 'TUK3_TS_MJ'
-THREADS = 8
 
 
 @app.route('/timeframe/<int:fgcid>/<int:frame>')
 def timeframe(fgcid, frame):
     with Cursor(SCHEMA_NAME) as cursor:
-        cursor.execute('call locations_at_timeframe({}, {}, ?)'.format(fgcid, frame))
+        cursor.execute('call locations_at_timeframe({}, {}, ?)'
+                       .format(fgcid, frame))
+        # TODO: adapt return type
+        return str(cursor.fetchall())
+
+
+@app.route('/timeframe_granularity/<int:fgcid>/<int:frame>/<int:granularity>')
+def timeframe_granularity(fgcid, frame, granularity):
+    # Granularity is supposed to be >= 0 and < 7
+    with Cursor(SCHEMA_NAME) as cursor:
+        cursor.execute('call locations_at_timeframe_granularity({}, {}, {}, ?)'
+                       .format(fgcid, frame, granularity))
         # TODO: adapt return type
         return str(cursor.fetchall())
 
