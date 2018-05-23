6343014679ae9b1ed38db81b05a1eb93c56fbc5b
Toni Stachewicz <toni.stachewicz@student.hpi.de>
2018-05-23 12:53:29 +0200

    moved statement to python file

--- server.py
+++ server.py
@@ -2,7 +2,6 @@ from cursor import Cursor
 from flask import Flask, render_template, Response
 import json
 
-
 app = Flask(__name__)
 
 SCHEMA_NAME = 'TUK3_TS_MJ'
@@ -12,12 +11,18 @@ SCHEMA_NAME = 'TUK3_TS_MJ'
 def index():   
     return render_template('map.html')
 
+
 @app.route('/timeframe/<int:fgcid>/<int:frame>')
 def timeframe(fgcid, frame):
     with Cursor(SCHEMA_NAME) as cursor:
-        cursor.execute('call locations_at_timeframe({}, {}, ?)'
-                       .format(fgcid, frame))
-        # TODO: adapt return type
+        lat = 'PF{}PX'.format(frame)
+        lon = 'PF{}PY'.format(frame)
+        query = '''select {0} + ify as lat, {1} + ifx as lon
+                   from "TUK3_TS_MJ"."FRAMEFORMAT2"
+                   where fgcid = {2}
+                   and {0} is not NULL'''.format(lat, lon, fgcid)
+
+        cursor.execute(query)
         return Response(json.dumps(cursor.fetchall()), mimetype='application/json')
 
 
@@ -25,10 +30,17 @@ def timeframe(fgcid, frame):
 def timeframe_granularity(fgcid, frame, granularity):
     # Granularity is supposed to be >= 0 and < 7
     with Cursor(SCHEMA_NAME) as cursor:
-        cursor.execute('call locations_at_timeframe_granularity({}, {}, {}, ?)'
-                       .format(fgcid, frame, granularity))
-        # TODO: adapt return type
-        #return str(cursor.fetchall())
+        lat = 'PF{}PX'.format(frame)
+        lon = 'PF{}PY'.format(frame)
+        query = '''select lat, lon, count(*) as weight from (
+                            select round({0} + ify, {1}) as lat,
+                                   round({2} + ifx, {1}) as lon
+                            from "TUK3_TS_MJ"."FRAMEFORMAT2"
+                            where fgcid = {3}
+                            and {0} is not NULL)
+                        group by lat, lon'''.format(lat, granularity, lon, fgcid)
+
+        cursor.execute(query)
         return Response(json.dumps(cursor.fetchall()), mimetype='application/json')
 
 
