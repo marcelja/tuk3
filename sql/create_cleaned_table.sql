9b148c1ae154dba452cf1348cd3919c9a33a3aac
Toni Stachewicz <toni.stachewicz@student.hpi.de>
2018-05-28 16:30:37 +0200

    sql script added

---
+++ sql/create_cleaned_table.sql
@@ -0,0 +1,18 @@
+drop table "TUK3_TS_MJ"."SHENZHEN_CLEAN" ;
+create column table SHENZHEN_CLEAN like "TAXI"."SHENZHEN";
+
+insert into "TUK3_TS_MJ"."SHENZHEN_CLEAN"(
+select * from "TAXI"."SHENZHEN"
+where id not in ( select id from "TUK3_TS_MJ"."SHENZHEN_CLEAN" group by id  having count(id) < 500)
+and id not in ( 
+    select id from (
+        select id, count( occupancy) as c 
+        from "TUK3_TS_MJ"."SHENZHEN_CLEAN" 
+        where occupancy = 1 
+        group by id , occupancy
+        ) 
+    where c < 120)
+and lon < 116
+and lon > 113
+and lat > 22
+and lat < 23.5);
