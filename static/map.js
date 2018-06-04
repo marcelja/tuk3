d5df6bb837258414da1d206069f8070a77d52e11
Marcel Jankrift <marcel@jankrift.de>
2018-06-04 16:59:42 +0200

    change default gradient and radius

--- static/map.js
+++ static/map.js
@@ -5,6 +5,23 @@
 var map, heatmap, paths = [], markers = [];
 var play = false;
 
+var gradient = [
+  'rgba(0, 255, 255, 0)',
+  'rgba(0, 255, 255, 1)',
+  'rgba(0, 191, 255, 1)',
+  'rgba(0, 127, 255, 1)',
+  'rgba(0, 63, 255, 1)',
+  'rgba(0, 0, 255, 1)',
+  'rgba(0, 0, 223, 1)',
+  'rgba(0, 0, 191, 1)',
+  'rgba(0, 0, 159, 1)',
+  'rgba(0, 0, 127, 1)',
+  'rgba(63, 0, 91, 1)',
+  'rgba(127, 0, 63, 1)',
+  'rgba(191, 0, 31, 1)',
+  'rgba(255, 0, 0, 1)'
+  ]
+
 window.onload = function() {
   loadScript();
   initSlider();
@@ -78,7 +95,9 @@ function initMap() {
   });
 
   heatmap = new google.maps.visualization.HeatmapLayer({
-    map: map
+    map: map,
+    gradient: gradient,
+    radius: 20
   });
 }
 
@@ -120,22 +139,6 @@ function showHeatmap() {
 }
 
 function changeGradient() {
-  var gradient = [
-  'rgba(0, 255, 255, 0)',
-  'rgba(0, 255, 255, 1)',
-  'rgba(0, 191, 255, 1)',
-  'rgba(0, 127, 255, 1)',
-  'rgba(0, 63, 255, 1)',
-  'rgba(0, 0, 255, 1)',
-  'rgba(0, 0, 223, 1)',
-  'rgba(0, 0, 191, 1)',
-  'rgba(0, 0, 159, 1)',
-  'rgba(0, 0, 127, 1)',
-  'rgba(63, 0, 91, 1)',
-  'rgba(127, 0, 63, 1)',
-  'rgba(191, 0, 31, 1)',
-  'rgba(255, 0, 0, 1)'
-  ]
   heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
 }
 
