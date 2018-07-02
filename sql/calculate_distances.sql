28b6daee42f18a8edcfa772347e3dbe642d7bde7
Toni Stachewicz <toni.stachewicz@student.hpi.de>
2018-07-02 12:13:57 +0200

    proc for table with st points

--- sql/calculate_distances.sql
+++ sql/calculate_distances.sql
@@ -96,3 +96,35 @@ select sum_table.id, sum_distance*2.4/1000+(sum_seconds*0.1*48/3600)+11*num_ride
     group by t1.id
 ) num_rides
 where sum_table.id=num_rides.id
+
+-- with st_point table
+select sum_table.id, sum_distance*2.4/1000+(sum_seconds*0.1*48/3600)+11*num_rides as profit, sum_distance*2.4/1000 as profit_distance, sum_seconds*0.1*48/3600 as profit_waiting, 11*num_rides as profit_taxi_start from
+
+----- calculate sum of distances and sum of seconds for each id
+(
+    select sum(distance) as sum_distance,sum(scnds) as sum_seconds,id from
+    (
+    select t1.id, t2.seconds-t1.seconds as scnds, t1.point.st_distance(t2.point) as distance from 
+    (select point, seconds,id,occupancy, rid from shenzhen_stpoints
+    -- where id = 22223
+    order by seconds) t1,
+    (select point, seconds,id,occupancy, rid from shenzhen_stpoints 
+    -- where id = 22223
+    order by seconds) t2
+    where t1.rid = t2.rid-1 and t1.occupancy=1 and t2.occupancy=1 and t1.id=t2.id
+    ) group by id order by sum_distance desc
+) sum_table,
+
+----- calculate number of rides
+(
+    select t1.id, count(t1.id) as num_rides from 
+    (select point, seconds,id,occupancy, rid from shenzhen_stpoints
+    -- where id = 22223
+    order by seconds) t1,
+    (select point, seconds,id,occupancy, rid from shenzhen_stpoints 
+    -- where id = 22223
+    order by seconds) t2
+    where t1.rid = t2.rid-1 and t1.occupancy=1 and t2.occupancy=0 and t1.id=t2.id
+    group by t1.id
+) num_rides
+where sum_table.id=num_rides.id
